4bb2b196d5e39e05cf49b4060487ac25b1698485
charlierehder <charlesreh1577@gmail.com>
2022-04-14 13:19:30 -0500

    added base elixir logic

--- wizardry-api.py
+++ wizardry-api.py
@@ -1,6 +1,20 @@
 from dataclasses import dataclass
+import requests
+import json
+from typing import List
 
 #using Wizard World API at https://wizard-world-api.herokuapp.com/swagger/index.html
+
+@dataclass
+class Wizard:
+    wizard_id: str
+    first_name: str
+    last_name: str
+
+@dataclass
+class Ingredient:
+    ingredient_id: str
+    name: str
 
 @dataclass
 class Elixir:
@@ -27,10 +41,6 @@ class House:
     common_room: str
     heads: List[Wizard]
     
-@dataclass
-class Ingredient:
-    ingredient_id: str
-    name: str
     
 @dataclass
 class Spell:
@@ -43,9 +53,22 @@ class Spell:
     light: str
     creator: str
     
-@dataclass
-class Wizard:
-    wizard_id: str
-    first_name: str
-    last_name: str
-    elixirs: List[Elixir]
+
+elixir_url = 'https://wizard-world-api.herokuapp.com/Elixirs'
+response = requests.get(elixir_url)
+data = json.loads(response)
+for key, value in data.iteritems():
+
+
+elixirs = []
+for row in data:
+    elixir_id = row['elixir_id']
+    name = row['name']
+    effect = row['effect']
+    side_effects = row['side_effects']
+    characteristic = row['characteristics']
+    time = row['time']
+    difficulty = row['difficulty']
+    ingredients = [Ingredient(ing_row['ingredient_id'],ing_row['name']) for ing_row in row['ingredients']] 
+    ingredients = [Wizard(ing_row['ingredient_id'],ing_row['name']) for wiz_row in row['inventors']]
+    elixirs.append(Elixir(elixir_id, name, effect, side_effects)
